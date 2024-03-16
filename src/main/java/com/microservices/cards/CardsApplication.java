ad9f6918c05ca283ce4a7da4d9b0330f5ae6575c
guttula Rohit <rohitguttula228@gmail.com>
2024-03-16 16:52:47 +0530

    added swagger configuration

--- src/main/java/com/microservices/cards/CardsApplication.java
+++ src/main/java/com/microservices/cards/CardsApplication.java
@@ -1,9 +1,34 @@
 package com.microservices.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "cards Microservice Rest Api documentation",
+				description = "Eazybank Cards Microservice Documentation",
+				version = "V1",
+				contact = @Contact(
+						name = "Rohit Guttula",
+						email="rohitguttula228@gmail.com",
+						url = "www.rohit.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.eazybytes.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank cards microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
